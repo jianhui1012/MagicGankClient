d7ef34bd2f82f908e81388d77ebd1bcf6edb6a5a
jianhui1012 <golikeholdon@gmail.com>
2018-05-04 22:47:34 +0800

    update

--- app/src/main/java/com/golike/magicgankclient/ui/fragment/RecommendByDayFragment.java
+++ app/src/main/java/com/golike/magicgankclient/ui/fragment/RecommendByDayFragment.java
@@ -1,9 +1,10 @@
 package com.golike.magicgankclient.ui.fragment;
 
 import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,6 +22,8 @@ import com.jude.easyrecyclerview.decoration.DividerDecoration;
 import com.jude.rollviewpager.RollPagerView;
 import com.jude.rollviewpager.hintview.ColorPointHintView;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 
 
@@ -32,10 +35,9 @@ public class RecommendByDayFragment extends BaseFragment<IRecommendByDayView, Re
     @BindView(R.id.recyclerView)
     EasyRecyclerView recyclerView;
     private RecyclerArrayAdapter<SearchData.GANK> adapter;
-    private int index = 1;
-    private String type;
-    private LinearLayoutManager layoutManager;
 
+    final String[] titles = {"干货闲读", "每日干货推荐", "新电影热映榜"};
+    final int[] icons = { R.mipmap.home_icon_fm,R.mipmap.home_icon_day,R.mipmap.home_icon_rank};
 
     @Override
     public int getLayoutId() {
@@ -49,7 +51,61 @@ public class RecommendByDayFragment extends BaseFragment<IRecommendByDayView, Re
 
     @Override
     public void initView() {
+        if (adapter == null) {
+            recyclerView.setAdapter(adapter = new RecyclerArrayAdapter<SearchData.GANK>(getActivity()) {
+                @Override
+                public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+                    return new CommonListHolder(parent);
+                }
+            });
+            recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+            DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY, Util.dip2px(getActivity(), 0.5f), Util.dip2px(getActivity(), 8), 0);
+            itemDecoration.setDrawLastItem(true);
+            itemDecoration.setDrawHeaderFooter(true);
+            recyclerView.addItemDecoration(itemDecoration);
+            recyclerView.setRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    recyclerView.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.clear();
+                            adapter.addAll(new ArrayList<>());
+                        }
+                    }, 1500);
+                }
+            });
+            adapter.addHeader(new RecyclerArrayAdapter.ItemView() {
+                @Override
+                public View onCreateView(ViewGroup parent) {
+                    View root = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_recommendbyday_listview_header, parent, false);
+                    RollPagerView rollpagerview = root.findViewById(R.id.rollpagerview);
+                    TabLayout recommendTabLayout = root.findViewById(R.id.recommendTabLayout);
+                    rollpagerview.setHintView(new ColorPointHintView(getActivity(), Color.YELLOW, Color.GRAY));
+                    rollpagerview.setPlayDelay(2000);
+                    rollpagerview.setAdapter(new BannerAdapter(getActivity()));
+                    for(int i=0;i<titles.length;i++){
+                        TabLayout.Tab tab = recommendTabLayout.newTab();
+                        tab.setText(titles[i]);
+                        tab.setIcon(icons[i]);
+                        recommendTabLayout.addTab(tab);
+                    }
+                    return root;
+                }
 
+                @Override
+                public void onBindView(View headerView) {
+
+                }
+            });
+            adapter.addAll(new ArrayList<>());
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        adapter = null;
     }
 
     @Override
@@ -59,63 +115,6 @@ public class RecommendByDayFragment extends BaseFragment<IRecommendByDayView, Re
 
     @Override
     public void showPage(SearchData searchData) {
-        if (adapter == null) {
-            adapter = new RecyclerArrayAdapter<SearchData.GANK>(getActivity()) {
-                @Override
-                public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
-                    return new CommonListHolder(parent);
-                }
-            };
-            layoutManager = new LinearLayoutManager(mContext);
-            recyclerView.setLayoutManager(layoutManager);
-            DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY, Util.dip2px(getActivity(), 0.5f), Util.dip2px(getActivity(), 8), 0);
-            itemDecoration.setDrawLastItem(true);
-            itemDecoration.setDrawHeaderFooter(true);
-            recyclerView.addItemDecoration(itemDecoration);
-            recyclerView.setAdapterWithProgress(adapter);
-            adapter.addHeader(new RecyclerArrayAdapter.ItemView() {
-                @Override
-                public View onCreateView(ViewGroup parent) {
-                    RollPagerView header = new RollPagerView(getActivity());
-                    header.setHintView(new ColorPointHintView(getActivity(), Color.YELLOW, Color.GRAY));
-                    //header.setHintPadding(0, 0, 0, (int) Utils.convertDpToPixel(8, getActivity()));
-                    header.setPlayDelay(2000);
-                    //header.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) Util.convertDpToPixel(200, getActivity())));
-                    header.setAdapter(null);
-                    return header;
-                }
 
-                @Override
-                public void onBindView(View headerView) {
-
-                }
-            });
-            adapter.addHeader(new RecyclerArrayAdapter.ItemView() {
-                @Override
-                public View onCreateView(ViewGroup parent) {
-                    RecyclerView recyclerView = new RecyclerView(parent.getContext()) {
-                        //为了不打扰横向RecyclerView的滑动操作，可以这样处理
-                        @Override
-                        public boolean onTouchEvent(MotionEvent event) {
-                            super.onTouchEvent(event);
-                            return true;
-                        }
-                    };
-//                    recyclerView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,(int) Utils.convertDpToPixel(300, HeaderFooterActivity.this)));
-//                    final NarrowImageAdapter adapter;
-//                    recyclerView.setAdapter(adapter = new NarrowImageAdapter(parent.getContext()));
-//                    recyclerView.setLayoutManager(new LinearLayoutManager(parent.getContext(),LinearLayoutManager.HORIZONTAL,false));
-//                    recyclerView.addItemDecoration(new SpaceDecoration((int) Utils.convertDpToPixel(8,parent.getContext())));
-//
-                    return recyclerView;
-                }
-
-                @Override
-                public void onBindView(View headerView) {
-                    //这里的处理别忘了
-                    ((ViewGroup) headerView).requestDisallowInterceptTouchEvent(true);
-                }
-            });
-        }
     }
 }
